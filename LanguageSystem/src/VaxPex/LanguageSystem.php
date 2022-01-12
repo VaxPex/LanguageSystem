3b98044db50434b4d583767d995f448cae59dad8
VaxPex <97077459+VaxPex@users.noreply.github.com>
2022-01-12 09:09:29 +0300

    better logic for the future

--- LanguageSystem/src/VaxPex/LanguageSystem.php
+++ LanguageSystem/src/VaxPex/LanguageSystem.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace VaxPex;
 
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
@@ -24,22 +23,6 @@ class LanguageSystem extends PluginBase implements Listener{
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 
-	public static function getPlayerLanguage(Player $player)
-	{
-		return $player->getLocale();
-	}
-
-	public function onJoin(PlayerJoinEvent $event){
-		$player = $event->getPlayer();
-		if(self::getPlayerLanguage($player) == "de_DE"){
-			$player->sendMessage(self::translateString("de", "lang_change") . ": " . self::getPlayerLanguage($player));
-		}else
-		if(self::getPlayerLanguage($player) == "ru_RU"){
-			$player->sendMessage(self::translateString("ru", "lang_change") . ": " . self::getPlayerLanguage($player));
-		}else
-		$player->sendMessage("Your language has been changed to: " . $player->getLocale());
-	}
-
 	public function saveKnownLanguages(){
 		$list = ["de_DE.json", "ru_RU.json"];
 		foreach($list as $item => $asstr){
@@ -47,13 +30,19 @@ class LanguageSystem extends PluginBase implements Listener{
 		}
 	}
 
+	public static function getPlayerLanguage(Player $player)
+	{
+		return $player->getLocale();
+	}
+
+	/**
+	 * @throws \ErrorException
+	 */
 	public static function translateString(string $lang, string $k){
-		if($lang == "de"){
+		if(!is_file(self::getInstance()->getDataFolder().$lang.".json")){
+			throw new \ErrorException("cant find {$lang} as a known language");
+		}else{
 			$config = new Config(self::getInstance()->getDataFolder() . "de_DE.json");
-			return $config->get($k);
-		}
-		if($lang == "ru"){
-			$config = new Config(self::getInstance()->getDataFolder() . "ru_RU.json");
 			return $config->get($k);
 		}
 	}
