9ef8e0c392807ce51b659df5e72755258868ee9c
VaxPex <97077459+VaxPex@users.noreply.github.com>
2022-01-19 10:23:28 +0300

    Update LanguageSystem.php

--- LanguageSystem/src/VaxPex/LanguageSystem.php
+++ LanguageSystem/src/VaxPex/LanguageSystem.php
@@ -6,21 +6,10 @@ namespace VaxPex;
 
 use pocketmine\event\Listener;
 use pocketmine\Player;
-use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
 
-class LanguageSystem extends PluginBase{
-
-	public function onEnable(){
-		$this->saveKnownLanguages();
-	}
-
-	public function saveKnownLanguages(){
-		$list = ["de_DE.json", "ru_RU.json"];
-		foreach($list as $item => $asstr){
-			$this->saveResource($asstr);
-		}
-	}
+class LanguageSystem
+{
 
 	public static function getPlayerLanguage(Player $player)
 	{
